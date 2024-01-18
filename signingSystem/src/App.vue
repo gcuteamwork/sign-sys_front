8fbc9c1507bd706213ecf6013db148a8fb406ad9
MG <2067979123@qq.com>
2024-01-18 22:25:00 +0800

    merge

--- signingSystem/src/App.vue
+++ signingSystem/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import login from "../src/views/login.vue";
-import register from "../src/views/register.vue";
+// import register from "../src/views/register.vue";
 </script>
 
 <template>
-  <!-- <login /> -->
-  <register />
+  <login />
+  <!-- <register /> -->
   <!-- <router-view /> -->
 </template>
