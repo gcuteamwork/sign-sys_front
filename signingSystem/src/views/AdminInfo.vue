62829807617599aca43913fa15fa6408be2f03ea
MG <2067979123@qq.com>
2024-01-22 22:14:13 +0800

    merge develop

--- signingSystem/src/views/AdminInfo.vue
+++ signingSystem/src/views/AdminInfo.vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
-    import DefaultHead from '../layouts/Header.vue'
-    import DefaultFoot from '../layouts/Footer.vue'
+import DefaultHead from "../layouts/Header.vue";
+import DefaultFoot from "../layouts/Footer.vue";
 </script>
 <template>
-   <div class="common-layout">
-      <el-container class="common-layout">
-      <el-header style="padding: 0;"><default-head /></el-header>
+  <div class="common-layout">
+    <el-container class="common-layout">
+      <el-header style="padding: 0"><default-head /></el-header>
       <el-main>main</el-main>
-      <el-footer style="position: fixed;bottom: 26px;width: 100%;"><default-foot /></el-footer>
+      <el-footer style="position: fixed; bottom: 26px; width: 100%"
+        ><default-foot
+      /></el-footer>
     </el-container>
   </div>
 </template>
